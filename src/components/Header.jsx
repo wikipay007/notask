3da4d0fa4517c457e75909f9567f5f6bb98d40a8
wikipay007 <kauansantosed@gmail.com>
2025-03-15 13:31:49 -0300

    Rota atualizada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { GoArrowRight } from "react-icons/go";
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate
 
 function Header() {
+  const navigate = useNavigate(); // Use useNavigate para obter a função de navegação
   return (
     <header>
-        <div className="imgLogo">
+      <div className="imgLogo">
         <img src="./img/notaskLogoBranco.png" alt="logoImg" />
-        </div>
-        <div className="links">
-            <a href="#">O projeto</a>
-            <a href="#">Funcionalidades</a>
-            <a href="#">Novidades</a>
-        </div>
-        <div className="buttons">
-            <button className='login'>Entre</button>
-            <button className='cadastro'>Cadastre-se <GoArrowRight/> </button>
-        </div>
-        
+      </div>
+      <div className="links">
+        <a href="#">O projeto</a>
+        <a href="#">Funcionalidades</a>
+        <a href="#">Novidades</a>
+      </div>
+      <div className="buttons">
+        <button className='login' onClick={() => navigate('/login')}>Entre</button> {/* Adicione o onClick para redirecionar */}
+        <button className='cadastro'>Cadastre-se <GoArrowRight /> </button>
+      </div>
+
     </header>
   )
 }
