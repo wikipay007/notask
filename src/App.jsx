d560f99914f0499ff374bd6c832e70a906405d40
wikipay007 <kauansantosed@gmail.com>
2025-03-15 13:09:38 -0300

    Tudo certo com branch kayque

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'; // Crie este componente se necessário
-import Login from './pages/login';
+import Login from '../src/pages/login';
 
 
 function App() {
